028beea2105237b1674b770c2f01fa4221ce6b0f
José Antonio Quintero Maya <a10joseqm@ATEIS.LOCAL>
2022-12-05 18:19:01 +0100

    Probando paso por valor y referencia

--- src/Clase.java
+++ src/Clase.java
@@ -2,19 +2,21 @@
  * @Author: José Antonio Quintero Maya
  */
 class Clase {
-    int entero;
-    String string;
-
+    int i;
+    String string = "string1";
 
     public Clase(int i, String string) {
-        this.entero = i;
+        this.i = i;
         this.string = string;
     }
 
-
+    public void modificaObjeto(int nuevo_int, String nuevo_string) {
+        i = nuevo_int;
+        string = nuevo_string;
+    }
 
     @Override
     public String toString() {
-        return "Objeto { i=" + entero + ", a='" + string + "\'}";
+        return "Objeto { i=" + i + ", a='" + string + "\'}";
     }
 }
